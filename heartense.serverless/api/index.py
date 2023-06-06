a2d64c3d24ccefd55b1df67292e5dde60b15c7c5
Kate <katie.lewis@gmail.com>
2023-06-06 16:54:15 +1000

    Trying to fix cors

--- heartense.serverless/api/index.py
+++ heartense.serverless/api/index.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import cross_origin
 
 app = Flask(__name__)
-CORS(app)
+
 
 @app.route('/')
+@cross_origin()
 def test():
     return jsonify({'message': 'Hello, World!'}), 200
