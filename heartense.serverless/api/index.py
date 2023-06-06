b33b95671305153638b797ada4b4ac3238995de2
Kate <katie.lewis@gmail.com>
2023-06-06 12:58:21 +1000

    deployment fix

--- heartense.serverless/api/index.py
+++ heartense.serverless/api/index.py
@@ -4,6 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/test', methods=['GET'])
+@app.route('/')
 def test():
     return jsonify({'message': 'Hello, World!'}), 200
